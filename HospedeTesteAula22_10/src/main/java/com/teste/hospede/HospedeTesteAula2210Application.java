ab2ee41aefd48113ee8edf722381b8a2a8980d71
biafmendes3110 <132359735+biafmendes3110@users.noreply.github.com>
2024-11-07 08:53:38 -0300

    Add files via upload

---
+++ HospedeTesteAula22_10/src/main/java/com/teste/hospede/HospedeTesteAula2210Application.java
@@ -0,0 +1,13 @@
+package com.teste.hospede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospedeTesteAula2210Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospedeTesteAula2210Application.class, args);
+	}
+
+}
---
+++ HospedeTesteAula22_10/src/test/java/com/teste/hospede/HospedeTesteAula2210ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.hospede;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospedeTesteAula2210ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
